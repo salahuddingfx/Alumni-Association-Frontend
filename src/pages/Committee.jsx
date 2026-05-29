d239e337c28fac3d30e9c5bf0c49aebf305dcdfe
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-30 00:02:52 +0600

    feat: add Committee page to display advisory and executive board members

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import api from '../api/api';
 import { Mail, Facebook, Linkedin } from 'lucide-react';
